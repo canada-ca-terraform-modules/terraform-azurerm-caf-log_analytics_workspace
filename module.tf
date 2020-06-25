55393af6542b9e0324d92b62e841fb22426d5983
Bernard Maltais <bmaltais@gmail.com>
2020-06-25 11:09:38 +0000

    Update variabler and code fmt

--- module.tf
+++ module.tf
@@ -2,13 +2,14 @@ locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   }
-  tags                            = merge(var.tags, local.module_tag)
-  unique                          = substr(sha1(var.resource_group.id), 0, 8)
-  log_analytics_workspace-replace = replace("${var.env}CLD-${var.userDefinedString}", "_", "-")
-  log_analytics_workspace-regex   = regex("[0-9A-Za-z-]+", local.log_analytics_workspace-replace)
-  log_analytics_workspace-54      = substr(local.log_analytics_workspace-regex, 0, 54)
-  log_analytics_workspace-59      = substr("${local.log_analytics_workspace-54}-${local.unique}", 0, 59)
-  log_analytics_workspace-result  = "${local.log_analytics_workspace-59}-law"
+  tags                                    = merge(var.tags, local.module_tag)
+  unique                                  = substr(sha1(var.resource_group.id), 0, 8)
+  env_4                                   = substr(var.env, 0, 4)
+  log_analytics_workspace-no_underscore   = replace("${local.env_4}CLD-${var.userDefinedString}", "_", "-")
+  log_analytics_workspace-regex_compliant = regex("[0-9A-Za-z-]+", local.log_analytics_workspace-no_underscore)
+  log_analytics_workspace-54              = substr(local.log_analytics_workspace-regex_compliant, 0, 54)
+  log_analytics_workspace-59              = substr("${local.log_analytics_workspace-54}-${local.unique}", 0, 59)
+  log_analytics_workspace-result          = "${local.log_analytics_workspace-59}-law"
 }
 
 //Can't have a "_" in the name, only "-"
--- variables.tf
+++ variables.tf
@@ -2,34 +2,25 @@ variable "resource_group" {
   description = "(Required) Resource group object of where the LAW is to be created"
 }
 
-variable "env" {
-  description = "(Required) 4 chars env name"
-  type        = string
-  default     = ""
-}
-
 variable "userDefinedString" {
   description = "(Required) userDefinedString value"
   type        = string
   default     = ""
 }
 
-variable "resource_group" {
-  description = "(Required) Resource group object of where the LAW is to be created"
-}
-
 variable "sku" {
   description = "(Optional) sku name"
+  type        = string
   default     = "PerGB2018"
-}
-
-variable "tags" {
-  description = "(Required) tagging for the log analytics workspace"
 }
 
 variable "retention_in_days" {
   description = " (Optional) The workspace data retention in days. Possible values are either 7 (Free Tier only) or range between 30 and 730."
   type        = string
   default     = ""
+}
+
+variable "tags" {
+  description = "(Required) tagging for the log analytics workspace"
 }
 
